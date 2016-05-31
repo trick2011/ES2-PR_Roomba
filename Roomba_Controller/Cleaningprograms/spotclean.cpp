7097858550aa05eb74d0dce8b6e8f41d26afbd16
Xinq- <jellelolkema@icloud.com>
2016-05-31 14:03:47 +0200

    small changes to spotclean and interpreter

--- Roomba_Controller/Cleaningprograms/spotclean.cpp
+++ Roomba_Controller/Cleaningprograms/spotclean.cpp
@@ -9,14 +9,19 @@ void Spotclean::clean(void)
     {
         interpreterreference.turnRoomba(1);
 
-        Run |= interpreterreference.Bumps.bLeft;
+        /*Run |= interpreterreference.Bumps.bLeft;
         Run |= interpreterreference.Bumps.bRight;
         Run |= interpreterreference.Cliff.bFrontLeft;
         Run |= interpreterreference.Cliff.bFrontRight;
         Run |= interpreterreference.Cliff.bLeft;
-        Run |= interpreterreference.Cliff.bRight;
+        Run |= interpreterreference.Cliff.bRight;*/ //deze alleen gebruiken als interpreter een thread is.
 
-        interpreterreference.brushes(roomba::brush::MAINBRUSH);
+        Run = interpreterreference.getBumpRight()       | interpreterreference.getBumpLeft()
+            | interpreterreference.getCliffLeft()       | interpreterreference.getCliffFrontLeft()
+            | interpreterreference.getCliffFrontRight() | interpreterreference.getCliffRight();
+
+
+        interpreterreference.brushes(roomba::brush::BOTH);
 
 
         while(Run == false && getEnableCleaning()== true);
--- Roomba_Interpreter/interpreter.h
+++ Roomba_Interpreter/interpreter.h
@@ -12,7 +12,7 @@
 
 #include "opcodes.h"
 #include "../Roomba_UART/uart.h"
-//#include "fail_error.h"
+#include "fail_error.h"
 
 #define debug
 #define fulldebug
@@ -39,51 +39,51 @@ public:
 
     void failSave();
 
-    bool getBumpAndWheel();
-    uint8_t getWall();
-    uint8_t getCliffLeft();
-    uint8_t getCliffFrontLeft();
-    uint8_t getCliffFrontRight();
-    uint8_t getCliffRight();
-    uint8_t getVirtualWall();
-    bool getWheelOvercurrents();
-    uint8_t getDirtDetect();
-    uint8_t getIrReceiver();
-    int16_t getDistance();
-    uint16_t getAngle();
-    uint8_t getChargingState();
-    uint16_t getBatteryVoltage();
-    int16_t getBatteryCurrent();
-    int8_t getBatteryTemperature();
-    uint16_t getBatteryCharge();
-    uint16_t getBatteryCapacity();
-    uint16_t getWallSignal();
-    uint16_t getCliffLeftSignal();
-    uint16_t getCliffFrontLeftSignal();
-    uint16_t getCliffFrontRightSignal();
-    uint16_t getCliffRightSignal();
-    uint8_t getChargingSource();
-    uint8_t getOiMode();
-    uint8_t getSongNumber();
-    uint8_t getSongPlaying();
-    int16_t getRequestedVelocity();
-    int16_t getRequestedRadius();
-    int16_t getRequestedRightVelocity();
-    int16_t getRequestedLeftVelocity();
-    uint16_t getLeftEncoderCount();
-    uint16_t getRightEncoderCount();
-    bool getLightBumper();
-    uint16_t getLightBumpLeftSignal();
-    uint16_t getLightBumpFrontLeftSignal();
-    uint16_t getLightBumpCenterLeftSignal();
-    uint16_t getLightBumpCenterRightSignal();
-    uint16_t getLightBumpFrontRightSignal();
-    uint16_t getLightBumpRightSignal();
-    int16_t getLeftMotorCurrent();
-    int16_t getRightMotorCurrent();
-    int16_t getMainBrushMotorCurrent();
-    int16_t getSideBrushMotorCurrent();
-    uint8_t getStatis();
+    bool        getBumpAndWheel();
+    uint8_t     getWall();
+    uint8_t     getCliffLeft();
+    uint8_t     getCliffFrontLeft();
+    uint8_t     getCliffFrontRight();
+    uint8_t     getCliffRight();
+    uint8_t     getVirtualWall();
+    bool        getWheelOvercurrents();
+    uint8_t     getDirtDetect();
+    uint8_t     getIrReceiver();
+    int16_t     getDistance();
+    uint16_t    getAngle();
+    uint8_t     getChargingState();
+    uint16_t    getBatteryVoltage();
+    int16_t     getBatteryCurrent();
+    int8_t      getBatteryTemperature();
+    uint16_t    getBatteryCharge();
+    uint16_t    getBatteryCapacity();
+    uint16_t    getWallSignal();
+    uint16_t    getCliffLeftSignal();
+    uint16_t    getCliffFrontLeftSignal();
+    uint16_t    getCliffFrontRightSignal();
+    uint16_t    getCliffRightSignal();
+    uint8_t     getChargingSource();
+    uint8_t     getOiMode();
+    uint8_t     getSongNumber();
+    uint8_t     getSongPlaying();
+    int16_t     getRequestedVelocity();
+    int16_t     getRequestedRadius();
+    int16_t     getRequestedRightVelocity();
+    int16_t     getRequestedLeftVelocity();
+    uint16_t    getLeftEncoderCount();
+    uint16_t    getRightEncoderCount();
+    bool        getLightBumper();
+    uint16_t    getLightBumpLeftSignal();
+    uint16_t    getLightBumpFrontLeftSignal();
+    uint16_t    getLightBumpCenterLeftSignal();
+    uint16_t    getLightBumpCenterRightSignal();
+    uint16_t    getLightBumpFrontRightSignal();
+    uint16_t    getLightBumpRightSignal();
+    int16_t     getLeftMotorCurrent();
+    int16_t     getRightMotorCurrent();
+    int16_t     getMainBrushMotorCurrent();
+    int16_t     getSideBrushMotorCurrent();
+    uint8_t     getStatis();
 
     /* get individual sensor values */
     bool getBumpRight();
