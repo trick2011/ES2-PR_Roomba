ed05836c7dbe8b33cb4658a4c4d39a014d4f9c38
Xinq <git@jellelolkema.com>
2016-06-09 14:59:20 +0200

    log.h updated

--- Roomba_Logger/log.h
+++ Roomba_Logger/log.h
@@ -50,25 +50,7 @@ private:
     typelog type = DEBUG;
     inline string getLabel(typelog type);
 
-    inline void addTime(){
-        timeb time;
-        ftime(&time);
-        int hours = ((time.time/3600)%24+2);
-        if(time.dstflag) hours += 1;
-        hours += (time.timezone/60);
-        if(hours < 10) operator << (0);
-        operator << (hours) <<":";
-        int minutes = (time.time/60)%60;
-        if(minutes < 10) operator << (0);
-        operator << (minutes)<<":";
-        int seconds = time.time%60;
-        if(seconds < 10) operator << (0);
-        operator << (seconds)<<".";
-        int milliseconds = time.millitm;
-        if(milliseconds < 100) operator << (0);
-        if(milliseconds < 10) operator << (0);
-        operator << (milliseconds) <<" ";
-    }
+    inline void addTime();
 };
 
 #endif
