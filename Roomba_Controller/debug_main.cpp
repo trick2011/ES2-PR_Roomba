a71edb2c3c0d2043d2db540eec3deda4e3c491b2
trick2011 <tarickw@gmail.com>
2016-06-11 16:07:57 +0200

    KOM OP JONGENS WRM LEEST NIEMAND DE CODE?

--- Roomba_Controller/debug_main.cpp
+++ Roomba_Controller/debug_main.cpp
@@ -12,7 +12,7 @@ int main(void){
 	inter.startRoomba();
 	//inter.startAutoMode();
 	//inter.drives(roomba::speed::SLOW);
-	Roombacontroller room(room);
+	Roombacontroller room(inter); // kom op jongens heeft echt NIEMAND de code nagelezen? Hier declareerde ik een roombacontroller object met een INTERPRETER reference naar een ROOMBACONTROLLER object dat ik net declareer!
 	check_class checker(room,inter);
 	checker.pipe_checker();
 
